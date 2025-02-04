5d7848525b14d40c68b06f9b8ec8e980fd3ea416
ezo <emersonrana@gmail.com>
2025-02-04 08:15:58 -0800

    add request to channel homepage, soupify content and pull title of site

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import BeautifulSoup4
-import Selenium
+import bs4
+import selenium
 import requests
+from bs4 import BeautifulSoup
 
-youtube_channel = input("Please paste the link of the youtube channel: ")
+# youtube_channel = input("Please paste the link of the youtube channel: ")
 
+daily_val_home = requests.get("https://www.youtube.com/@DailyValorantqq")
+
+daily_val_content = daily_val_home.content
+
+soup = BeautifulSoup(daily_val_content, "html.parser")
+
+print(soup.title)
 
