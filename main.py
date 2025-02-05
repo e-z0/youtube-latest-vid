0065ead7b073e9d9c83b21fa0e6b60020fa84dfa
ezo <emersonrana@gmail.com>
2025-02-04 17:08:06 -0800

    add function to get link with fail checks

--- main.py
+++ main.py
@@ -3,13 +3,40 @@ import selenium
 import requests
 from bs4 import BeautifulSoup
 
-# youtube_channel = input("Please paste the link of the youtube channel: ")
+# youtube_channel = input("Please paste the link of the youtube channel's homepage: ")
 
-daily_val_home = requests.get("https://www.youtube.com/@DailyValorantqq")
+def get_video(channel_url):
 
-daily_val_content = daily_val_home.content
+    # Send GET request to channel URL
+    response = requests.get(channel_url)
 
-soup = BeautifulSoup(daily_val_content, "html.parser")
+    # Confirm request was successful
+    if response.status_code != 200:
+        print("Failed to retreive the page")
+        return None
 
-print(soup.title)
+    # Make soup with HTML parser
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    # Look for youtube specific layout for videos
+    # video = soup.find(id = "contents", class_ = "style-scope ytd-section-list-renderer")
+    video = soup.find("body")
+    # link = video.get("href")
+
+    print(video)
+    # print(link)
+
+    # if video:
+    #     # Find video URL
+    #     link = video.find("a", {"href": True})
+    #     if link:
+    #         video_url = "https://www.youtube.com" + link["href"]
+    #         return video_url
+    # return "No video found"
+
+recent_video = get_video("https://www.youtube.com/@DailyValorantqq")
+
+# if recent_video:
+#     print(f"The most recent video from [Channel Name] is: {recent_video}")
+# else:
+#     print("Could not find the most recent video.")
